4a1ce2875ce0f8b0f659b87a58f7f9db00fc36a6
Iaroslav Shcherbatyi <iaroslav.email@gmail.com>
2017-08-28 13:16:37 +0200

    Bunch of new nn models

--- noxer/rnn.py
+++ noxer/rnn.py
@@ -2,6 +2,7 @@ from sklearn.base import ClassifierMixin, RegressorMixin, BaseEstimator, Transfo
 from sklearn.preprocessing import LabelEncoder, FunctionTransformer
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
+from abc import abstractmethod
 
 import tempfile
 import numpy as np
@@ -56,7 +57,7 @@ class KerasNNBase(BaseEstimator):
 
     """
     def __init__(self, n_neurons=32, n_layers=1, lr=1e-4, beta1=0.9, beta2=0.99,
-                 batch_size=128, max_iter=128, max_patience=32, val_fraction=0.2):
+                 batch_size=128, max_iter=128, max_patience=1e10, val_fraction=0.2):
         self.n_neurons = n_neurons
         self.n_layers = n_layers
 
@@ -103,7 +104,10 @@ class KerasNNBase(BaseEstimator):
         y = self.encoder.transform(y)
 
         # split data into training and validation parts
-        X_train, X_val, y_train, y_val = train_test_split(X, y, train_size=(1.0 - self.val_fraction))
+        #X_train, X_val, y_train, y_val = train_test_split(X, y, train_size=(1.0 - self.val_fraction))
+
+        X_train = X
+        y_train = y
 
         best_loss_ = 100000000000.0
         patience = self.max_patience
@@ -114,18 +118,23 @@ class KerasNNBase(BaseEstimator):
         while patience > 0 and max_iter > 0:
             max_iter -= 1
 
-            self.model.fit(X_train,y_train, nb_epoch=1, batch_size=self.batch_size, verbose=0)
-            val_loss = self.model.evaluate(X_val, y_val, verbose=0)
+            val_loss = self.model.fit(X_train,y_train, nb_epoch=1, batch_size=self.batch_size, verbose=0)
+            val_loss = val_loss.history['loss'][-1]
+            #val_loss = self.model.evaluate(X_val, y_val, verbose=0)
 
             if np.isnan(val_loss) or np.isinf(val_loss):
                 break
 
+            best_model_ = [np.copy(w) for w in self.model.get_weights()]
+            max_iter -= 1
+
+            """
             if val_loss < best_loss_:
                 best_loss_ = val_loss
-                best_model_ = [np.copy(w) for w in self.model.get_weights()]
                 patience = self.max_patience
             else:
                 patience -= 1
+            """
 
         self.model.set_weights(best_model_)
 
@@ -137,12 +146,20 @@ class KerasNNBase(BaseEstimator):
     def predict(self, X):
         return self.encoder.inverse_transform(self._predict(X))
 
+def bad_activation(x):
+    return x - 3.0
 
-class RNNClassifier(KerasNNBase, ClassifierMixin):
+class KerasClassifierBase(KerasNNBase, ClassifierMixin):
+    @abstractmethod
+    def create_architecture(self, X, n_classes):
+        """
+        Generates the architecture of nn to be trained.
+        """
 
     def _make_model(self, X, y, optimizer):
         import keras.models
-        from keras.layers import Input, Dense, GRU
+        from keras.layers import Input, Dense, Conv1D, Flatten
+        from keras.layers import Activation
         from keras.optimizers import Adam
 
         n_classes = len(np.unique(y))
@@ -150,12 +167,17 @@ class RNNClassifier(KerasNNBase, ClassifierMixin):
         self.encoder.fit(y)
         y = self.encoder.transform(y)
 
-        ip = Input(shape=X[0].shape)
-        x = ip
-        x = GRU(self.n_neurons, activation="relu")(x)
-        x = Dense(n_classes)(x)
+        try:
+            model = self.create_architecture(X, n_classes)
+        except BaseException as ex:
+            ip = Input(shape=X[0].shape)
+            x = ip
+            x = Flatten()(x)
+            x = Dense(n_classes, activation='tanh')(x)
+            x = Activation(bad_activation)(x)
+            print('Infeasible!')
+            model = keras.models.Model(input=ip, output=x)
 
-        model = keras.models.Model(input=ip, output=x)
         model.compile(
             optimizer=optimizer,
             loss='sparse_categorical_crossentropy'
@@ -172,7 +194,65 @@ class RNNClassifier(KerasNNBase, ClassifierMixin):
         return np.argmax(yp, axis=1)
 
 
-class RNNRegressor(KerasNNBase, RegressorMixin):
+class RNNClassifier(KerasClassifierBase):
+    def create_architecture(self, X, n_classes):
+        import keras.models
+        from keras.layers import Input, Dense, GRU
+        from keras.layers.advanced_activations import LeakyReLU
+        ip = Input(shape=X[0].shape)
+        x = ip
+        for i in range(self.n_layers):
+            x = GRU(self.n_neurons)(x)
+        x = Dense(n_classes, activation='softmax')(x)
+        return keras.models.Model(input=ip, output=x)
+
+
+class CNN1DClassifier(KerasClassifierBase):
+    def __init__(self, conv_sz=3, stride=1, n_neurons=32, n_layers=1, lr=1e-4, beta1=0.9, beta2=0.99,
+                 batch_size=128, max_iter=128, max_patience=32, val_fraction=0.2):
+        super(CNN1DClassifier, self).__init__(
+            n_neurons=n_neurons, n_layers=n_layers, lr=lr, beta1=beta1, beta2=beta2,
+            batch_size=batch_size, max_iter=max_iter, max_patience=max_patience, val_fraction=val_fraction
+        )
+        self.conv_sz = conv_sz
+        self.stride = stride
+
+    def create_architecture(self, X, n_classes):
+        import keras.models
+        from keras.layers import Input, Dense, Conv1D, Flatten
+        from keras.layers.advanced_activations import LeakyReLU
+        ip = Input(shape=X[0].shape)
+        x = ip
+        for i in range(self.n_layers):
+            x = Conv1D(filters=self.n_neurons, kernel_size=self.conv_sz,
+                       strides=self.stride, padding='same')(x)
+            x = LeakyReLU(0.05)(x)
+        x = Flatten()(x)
+        x = Dense(n_classes, activation='softmax')(x)
+        return keras.models.Model(input=ip, output=x)
+
+
+class DNNClassifier(KerasClassifierBase):
+    def create_architecture(self, X, n_classes):
+        import keras.models
+        from keras.layers import Input, Dense, Flatten
+        from keras.layers.advanced_activations import LeakyReLU
+        ip = Input(shape=X[0].shape)
+        x = ip
+        x = Flatten()(x)
+        for i in range(self.n_layers):
+            x = Dense(self.n_neurons)(x)
+            x = LeakyReLU(0.05)(x)
+        x = Dense(n_classes, activation='softmax')(x)
+        model = keras.models.Model(input=ip, output=x)
+        return model
+
+class KerasRegressorBase(KerasNNBase, RegressorMixin):
+    @abstractmethod
+    def create_architecture(self, X):
+        """
+        Creates architecture of regressor.
+        """
 
     def _make_model(self, X, y, optimizer):
         import keras.models
@@ -180,20 +260,71 @@ class RNNRegressor(KerasNNBase, RegressorMixin):
         from keras.optimizers import Adam
 
         self.encoder = FunctionTransformer(func=lambda x: x, inverse_func=lambda x: x)
-        ip = Input(shape=X[0].shape)
-        x = ip
-        x = GRU(self.n_neurons, activation="relu")(x)
-        x = Dense(1)(x)
 
-        model = keras.models.Model(input=ip, output=x)
+        try:
+            model = self.create_architecture
+        except BaseException as ex:
+            ip = Input(shape=X[0].shape)
+            x = ip
+            x = Dense(1)(x)
+            model = keras.models.Model(input=ip, output=x)
+
         model.compile(
             optimizer=optimizer,
             loss='sparse_categorical_crossentropy'
         )
 
-
-
         self.model = model
 
     def _predict(self, X):
         return self.model.predict(X)
+
+class RNNRegressor(KerasRegressorBase):
+    def create_architecture(self, X):
+        import keras.models
+        from keras.layers import Input, Dense, GRU
+        from keras.layers.advanced_activations import LeakyReLU
+        ip = Input(shape=X[0].shape)
+        x = ip
+        for i in range(self.n_layers):
+            x = GRU(self.n_neurons)(x)
+            x = LeakyReLU(0.05)(x)
+        x = Dense(1)(x)
+        model = keras.models.Model(input=ip, output=x)
+        return model
+
+class CNN1DRegressor(KerasRegressorBase):
+    def __init__(self, conv_sz, stride, *args, **kwargs):
+        super(CNN1DRegressor, self).__init__(
+            *args, **kwargs
+        )
+        self.conv_sz = conv_sz
+        self.stride = stride
+
+    def create_architecture(self, X):
+        import keras.models
+        from keras.layers import Input, Dense, Conv1D, Flatten
+        from keras.layers.advanced_activations import LeakyReLU
+        ip = Input(shape=X[0].shape)
+        x = ip
+        for i in range(self.n_layers):
+            x = Conv1D(self.n_neurons, self.conv_sz, self.stride, padding='same')(x)
+            x = LeakyReLU(0.05)(x)
+        x = Flatten()(x)
+        x = Dense(1)(x)
+        model = keras.models.Model(input=ip, output=x)
+        return model
+
+class DNNRegressor(KerasRegressorBase):
+    def create_architecture(self, X):
+        import keras.models
+        from keras.layers import Input, Dense
+        from keras.layers.advanced_activations import LeakyReLU
+        ip = Input(shape=X[0].shape)
+        x = ip
+        for i in range(self.n_layers):
+            x = Dense(self.n_neurons)(x)
+            x = LeakyReLU(0.05)(x)
+        x = Dense(1)(x)
+        model = keras.models.Model(input=ip, output=x)
+        return model
